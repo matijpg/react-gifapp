5330a09cc510317ab1c531190e746f01aee29f8e
Matias Sierra <hernanmatiassierra@gmail.com>
2020-09-08 23:33:34 -0300

    docs agregado

---
+++ docs/precache-manifest.507fdd2836da379dc09ea434e1edef1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "875953eb60123872f148298096dc4adb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "075566375c312f00a87b",
+    "url": "/static/css/main.db1ceff5.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075566375c312f00a87b",
+    "url": "/static/js/main.c76dd578.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
